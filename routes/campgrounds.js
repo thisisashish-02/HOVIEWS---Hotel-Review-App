e4288d3a469edd2767db3322994b80423662ebb5
ASHISH <65739459+thisisashish-02@users.noreply.github.com>
2021-05-31 22:07:03 +0530

    added image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,13 +3,17 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-
-const Campground = require('../models/campground');
-const { route } = require('./users');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('IT WORKED!')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
